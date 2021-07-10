3e2ff343e2885f1054aa9c4653edcdb9d12259f8
Hitotsubashi <1220043823@qq.com>
2021-07-10 14:54:58 +0800

    完成thunk的异步action

--- store/action/index.js
+++ store/action/index.js
@@ -2,3 +2,9 @@ export const SET_EMOJIS=(emojis)=>({
   type:'SET_EMOJIS',
   emojis
 })
+
+export const REQUEST_EMOJIS = ()=>dispatch => (
+  fetch('https://api.github.com/emojis')
+    .then((res)=>res.json())
+    .then(emojis => dispatch(SET_EMOJIS(emojis)))
+)
