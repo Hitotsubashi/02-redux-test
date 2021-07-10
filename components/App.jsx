2b77743ec19bfada99c3745b53e13d55462e9e11
Hitotsubashi <1220043823@qq.com>
2021-07-10 14:04:38 +0800

    新增store

--- components/App.jsx
+++ components/App.jsx
@@ -1,8 +1,10 @@
 import  React  from 'react';
+import { connect } from 'react-redux'
 
 const App = (props)=>{
   const {emojis} = props
   return <div>
+    <h2>emojis</h2>
     {
       Object.entries(emojis).map(([key,value])=>{
         <img src={value} alt={key} title={key}/>
@@ -11,3 +13,8 @@ const App = (props)=>{
   </div>
 }
 
+const mapStateToProps = (state) => ({
+  emojis:state
+})
+
+export default  connect(mapStateToProps,null)(App)
---
+++ store/action/index.js
@@ -0,0 +1,4 @@
+export const SET_EMOJIS=(emojis)=>({
+  type:'SET_EMOJIS',
+  emojis
+})
