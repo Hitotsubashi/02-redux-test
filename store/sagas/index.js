c70c4b4883a91a670bb42888961364e0429ecdd5
wayneliang(梁宇浩) <wayneliang@futunn.com>
2021-08-03 21:09:50 +0800

    完成jest对saga的单元测试

--- store/sagas/index.js
+++ store/sagas/index.js
@@ -1,10 +1,10 @@
 import { call, put, takeEvery } from "redux-saga/effects";
 
-const fetchEmojis = async () => {
+export const fetchEmojis = async () => {
   return fetch("https://api.github.com/emojis").then((res) => res.json());
 };
 
-function* requestEmojis() {
+export function* requestEmojis() {
   const emojis = yield call(fetchEmojis);
   yield put({ type: "SET_EMOJIS", emojis });
 }
